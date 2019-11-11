c50c95354072294a1ab0696b130b3ec130c35491
ADobrzanski <avion.ms@live.com>
2019-11-11 19:42:18 +0100

    [iss2] Fix wrong fontawesome npm dependency

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faPlay,
+  faPause,
+  faStop,
+  faStepForward,
+  faStepBackward,
+} from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+library.add({
+  faPlay,
+  faPause,
+  faStop,
+  faStepForward,
+  faStepBackward,
+});
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
